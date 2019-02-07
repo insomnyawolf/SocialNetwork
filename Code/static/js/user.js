8fcf987dc3d8f0eddaa137b377ccd2b36390f156
insomnyawolf <ruben.tokyo1999@gmail.com>
2019-02-07 19:33:20 +0100

    User Settings Ajax

---
+++ Code/static/js/user.js
@@ -0,0 +1,30 @@
+function addAcount() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("addAccount").innerHTML = this.responseText;
+	  }/*else{
+        alert('Request failed.  Returned status of ' + xhttp.status);
+      }*/
+    };
+    xhttp.open("POST", "./code/settings.php", true);
+    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhttp.send(param({
+      'func': 'addAccount',
+    }));
+  return false;
+}
+//Parsear los datos a editar a datos de formulario
+function param(object) {
+  var encodedString = '';
+  for (var prop in object) {
+      if (object.hasOwnProperty(prop)) {
+          if (encodedString.length > 0) {
+              encodedString += '&';
+          }
+          encodedString += encodeURI(prop + '=' + object[prop]);
+      }
+  }
+  //alert(encodedString)
+  return encodedString;
+}
