9ce6141d53e852777430568743d07a7d9d8c56df
insomnyawolf <ruben.tokyo1999@gmail.com>
2019-02-11 21:05:59 +0100

    Query actualizada y nuevas tablas

--- Code/code/Sql query.sql
+++ Code/code/Sql query.sql
@@ -25,6 +25,28 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   FOREIGN KEY (`user_id`) REFERENCES users(user_id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+CREATE TABLE IF NOT EXISTS `indices` (
+  `indices_id` int(11) NOT NULL auto_increment,
+  `name` varchar(11) NOT NULL,
+  `value` decimal(12,2)DEFAULT 1,
+  `last_refresh` int(11)DEFAULT 0,
+  PRIMARY KEY  (`indices_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+INSERT INTO `indices`(`name`) values("AENA");
+INSERT INTO `indices`(`name`) values("IBERDROLA");
+INSERT INTO `indices`(`name`) values("INDITEX");
+
+CREATE TABLE IF NOT EXISTS `indices_compra` (
+  `indices_compra_id` int(11) NOT NULL auto_increment,
+  `indices_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) DEFAULT 0,
+  PRIMARY KEY  (`indices_compra_id`),
+  FOREIGN KEY (`user_id`) REFERENCES users(user_id),
+  FOREIGN KEY (`indices_id`) REFERENCES indices(indices_id)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
 CREATE TABLE IF NOT EXISTS `historico` (
   `trans_id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
